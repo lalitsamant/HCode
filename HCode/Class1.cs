343e6ab6ac27b9905289521e54173fd737c3933a
lalit <lalit@DESKTOP-UHHNV8C>
2023-02-09 21:07:51 +0000

    Add project files.

---
+++ HCode/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HCode
+{
+    public class Activity
+    {
+        public string ActRef { get; set; }
+        public string ActDate { get; set; }
+        public string ActActivity { get; set; }
+    }
+
+    public class ActClass
+    {
+        public string ActRef { get; set; }
+        public string ActStartdate { get; set; }
+        public string ActEnddate { get; set; }
+        public string ActWeekPattern { get; set; }
+        public string Activity { get; set; }
+
+    }
+}
---
+++ HCode/Program.cs
@@ -0,0 +1,78 @@
+﻿using HCode;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+#region Startup
+string filePath = @"c:\hcode\Sample Activities.csv";
+IEnumerable<string> strCSV = File.ReadLines(filePath);
+
+var results = from str in strCSV
+              let tmp = str.Split(',')
+              select new ActClass
+              {
+                  ActRef = tmp[0],
+                  ActStartdate = tmp[1],
+                  ActEnddate = tmp[2],
+                  ActWeekPattern = tmp[3],
+                  Activity = tmp[4]
+              };
+
+var query = results.Skip(1).ToList();
+ArrayList ActList = new ArrayList();
+
+foreach (var x in query)
+{
+    string ARef = x.ActRef;
+    var WPlen = x.ActWeekPattern.Length;
+    var i = 1;
+    var ActDate = x.ActStartdate;
+    var ActSDate = x.ActStartdate;
+    var ActEDate = x.ActEnddate;
+    
+    ActList.Add(new Activity { ActRef = ARef, ActDate = ActSDate, ActActivity = x.Activity });
+    ActList.Add(new Activity { ActRef = ARef, ActDate = ActEDate, ActActivity = x.Activity });
+
+    while (i < WPlen-1) 
+    {
+        string WP = x.ActWeekPattern.Substring(i, 1);
+        if (!string.IsNullOrWhiteSpace(WP)) 
+        {
+            ActList.Add(new Activity { ActRef = ARef, ActDate = DateTime.Parse(ActDate).AddDays(7).ToString(), ActActivity = x.Activity });
+            ActDate = DateTime.Parse(ActDate).AddDays(7).ToString();
+        }
+        else
+        {
+            ActDate = DateTime.Parse(ActDate).AddDays(7).ToString();
+        }
+        i++;
+    }
+}
+
+foreach (var x in query)
+{
+    string ARef1 = x.ActRef;
+    string ASDate = x.ActStartdate;
+
+    List<Activity> list1 = ActList.Cast<Activity>().ToList();
+    Console.WriteLine("Activity :" + ARef1);
+    Console.WriteLine("Clashes With :");
+    foreach (var y in list1)
+    {
+        if (ARef1 == y.ActRef && ASDate == y.ActDate) 
+        {           
+        }
+        else 
+        {
+            if (ASDate == y.ActDate)
+            {
+                Console.WriteLine("     " + y.ActRef + " on " + y.ActDate + " - " + y.ActActivity);
+            }
+        }
+    }
+    Console.WriteLine("");
+}
+Console.WriteLine("_____________");
+Console.WriteLine("End of Report");
+#endregion
